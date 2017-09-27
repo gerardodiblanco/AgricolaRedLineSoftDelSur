ff6ed34c033b7fd46c0a08b1f54bf446e0195ca1
Gera <gerardo_dibli@hotmail.com>
2017-09-27 13:06:13 -0300

    nuevo cuartel

--- Agricola/src/app/components/cuartel/nuevo-cuartel/nuevo.cuartel.component.ts
+++ Agricola/src/app/components/cuartel/nuevo-cuartel/nuevo.cuartel.component.ts
@@ -23,18 +23,18 @@ export class NuevoCuartelComponent {
   cuarteles: any[] = [];
   cuartelSeleccionado: any = null;
   pathsCampo: Array<LatLngLiteral> = new Array<LatLngLiteral>();
-  arrayPaths: Array<Array<LatLngLiteral>> = [];
+  arrayPathsCuarteles: Array<Array<LatLngLiteral>> = [];
   markers: marker[];
   latInicio = -32.880913;
   lngInicio = -68.83319;
   editable: boolean;
-  
+
 
   constructor(private campoService: CampoService, private nuevoCampoService: NuevoCampoService,
-    private cuartelService: CuartelService,private route: ActivatedRoute) {
-      this.editable = false;
+    private cuartelService: CuartelService, private route: ActivatedRoute) {
+    this.editable = false;
 
-      this.buscarCuartel();
+    this.buscarCuartel();
   }
 
 
@@ -49,38 +49,55 @@ export class NuevoCuartelComponent {
           this.cuartelSeleccionado = cuartel;
           console.log("buscar cuartel");
           console.log(this.cuartelSeleccionado);
-          if (cuartel.idCampo == null) { this.editable = true; this.titulo = "Nuevo Cuartel" }
-          this.titulo += cuartel.descripcion;
+
+          if (cuartel.idCampo == null) {
+          this.editable = true; this.titulo = "Nuevo Cuartel"
+         //   this.buscarCampoPorId(id);
+          } else {
+            this.titulo += cuartel.descripcion;
+            this.buscarCampo(cuartel);
+          }
         }));
+  }
 
-        this.buscarCampo(this.cuartelSeleccionado);
-this.buscarCuarteles(this.campo);
-      }
-
-
-  
-  buscarCuarteles(campo: any) {
-    if(campo != null){
-          this.cuartelService.getCuarteles(campo.idCampo)
-          .then(cuarteles => {
-            this.cuarteles = cuarteles
-            console.log("cuarteles : ");
-            console.log(this.cuarteles);
-          })
-        }
-      }
- 
-
-  buscarCampo(cuartel: any) {
-    //buscar campo seleccionado
-    if(cuartel != null){
+  buscarCampoPorId(id: any) {
+    console.log("buscar por ID");
     this.nuevoCampoService
-      .buscarCampo(cuartel.idCampo)
+      .buscarCampo(id)
       .then(c => {
         this.campo = c;
         console.log("campo")
         console.log(this.campo);
+        this.buscarCuarteles(c);
       });
+  }
+
+
+  buscarCampo(cuartel: any) {
+    //buscar campo seleccionado
+    if (cuartel != null) {
+      this.nuevoCampoService
+        .buscarCampo(cuartel.idCampo)
+        .then(c => {
+          this.campo = c;
+          console.log("campo")
+          console.log(this.campo);
+          this.buscarCuarteles(c);
+        });
+    }
+  }
+
+  buscarCuarteles(campo: any) {
+    if (campo != null) {
+      this.cuartelService.getCuarteles(campo.idCampo)
+        .then(cuarteles => {
+          this.cuarteles = cuarteles
+          console.log("cuarteles : ");
+          console.log(this.cuarteles);
+          if (this.cuarteles) {
+            this.actualizarAreas();
+          }
+        });
     }
   }
 
@@ -88,17 +105,53 @@ this.buscarCuarteles(this.campo);
   actualizarAreas() {
     if (this.campo != null) {
       console.log("actualizar Areas Cuarteles ")
-      this.arrayPaths = [];
-      this.arrayPaths = convertElementoConCoordenadasToArrayPaths(this.cuarteles);
+      this.arrayPathsCuarteles = [];
+      this.arrayPathsCuarteles = convertElementoConCoordenadasToArrayPaths(this.cuarteles);
       this.pathsCampo = [];
       this.pathsCampo = convertCoordenadaListToPaths(this.campo.coordenadaList);
       this.actualizarMarker();
-      console.log(this.arrayPaths);
+      console.log(this.arrayPathsCuarteles);
     }
+
   }
+
+
+
+
 
   actualizarMarker() {
     this.markers = [];
-    this.markers = convertCoordenadaListToMarkerList(this.campo.coordenadaList)
+    this.markers = convertCoordenadaListToMarkerList(this.cuartelSeleccionado.coordenadaList)
   }
+
+  editar() {
+    this.editable = true;
+  }
+
+
+
+  posicionFinalMarcador(m, $event, i) {
+    this.markers[i].lati = $event.coords.lat;
+    this.markers[i].longi = $event.coords.lng;
+
+  }
+
+
+  mapClickeado($event: any) {
+    console.log("Mapa Clickeado");
+    if (this.editable) {
+      var nuevoMarker = {
+        nroOrden: this.markers.length + 1,
+        lati: $event.coords.lat,
+        longi: $event.coords.lng,
+        id: $event.id,
+      }
+      this.markers.push(nuevoMarker);
+      //this.actualizarPaths();
+    }
+  }
+
+
+
+
 }
